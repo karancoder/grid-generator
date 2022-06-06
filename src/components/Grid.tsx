72abaee3e13524795d51522b5286e403ff20e75a
kjivani <kjivani@cisco.com>
2022-06-06 20:50:25 +0530

    initial commit

---
+++ src/components/Grid.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Grid.scss';
+
+function Grid() {
+  return (
+    <div className="grid-container">
+        <div className="grid">
+            Grid
+        </div>
+    </div>
+  )
+}
+
+export default Grid
---
+++ src/components/GridController.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './GridController.scss';
+
+const GridController = () => {
+  return (
+    <div className="grid-controller">GridController</div>
+  )
+}
+
+export default GridController
